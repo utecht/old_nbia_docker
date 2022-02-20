ef4c09b1ab1174e7bf92ecc22227dcb7c2783a89
Joseph Utecht <jrutecht@uams.edu>
2022-02-20 13:06:38 -0600

    removing old start script

--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker compose up mysql -d
-docker compose up solr -d
-docker compose up openldap -d
-echo "Sleeping for 30 seconds to wait for database/solr to initialize"
-sleep 30
-docker compose up tomcat
